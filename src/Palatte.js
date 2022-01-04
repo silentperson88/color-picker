6f586282bca57d73a97d702ffd7768ad7f6e10f9
patel ravi <patelravi1102001@gmail.com>
2022-01-04 17:05:50 +0530

    react-color, first commit

---
+++ src/Palatte.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Palatte() {
+  return (
+    <div className="palette">
+      {/* Navbar goes here  */}
+      <div className="Palatte-colors">{/* bunch of color boxes */}</div>
+      {/* footer goes here */}
+    </div>
+  );
+}
+
+export default Palatte;
